71c0fb2bae8f62b074ec61e0b82fc4f6c9a6c558
susmitha <susmithathanneru555@gmail.com>
2026-03-10 16:23:02 +0530

    Improve Render deployment: add gunicorn workers/timeout, improve build script logging

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
-set -o errexit
+set -e
 
-pip install --upgrade pip
+echo "Installing pip..."
+pip install --upgrade pip setuptools wheel
+
+echo "Installing requirements..."
 pip install -r requirements.txt
+
+echo "Running migrations..."
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
+
+echo "Build script completed successfully!"
 python manage.py collectstatic --noinput
